01a2cf1319802a220ce574f1e2337d282a8dd1bd
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2026-01-23 23:43:33 +0100

    fix: removed the publish none tag

--- ui_playground/lib/src/ui_playground.dart
+++ ui_playground/lib/src/ui_playground.dart
@@ -18,15 +18,23 @@ class UiPlaygroundApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final optionalTheme = ImpaktfullUiTheme.maybeOf(context);
-    final impaktfullUiTheme =
-        optionalTheme ??
-        theme?.impaktfullUiTheme ??
-        ImpaktfullUiTheme.getDefault();
+    final app = ImpaktfullUiApp.maybeOf(context);
+    final impaktfullUiTheme = app?.impaktfullUiTheme ?? theme?.impaktfullUiTheme ?? ImpaktfullUiTheme.getDefault();
     UiPlaygroundTheme.configure(impaktfullUiTheme);
     return ImpaktfullUiApp(
-      title: title,
       impaktfullUiTheme: impaktfullUiTheme,
+      localizations: app?.localizations ?? const ImpaktfullUiLocalizations(),
+      locale: app?.locale,
+      materialDarkTheme: app?.materialDarkTheme,
+      materialLightTheme: app?.materialLightTheme,
+      overrideComponents: app?.overrideComponents ?? const [],
+      snackyBuilder: app?.snackyBuilder,
+      snackyController: app?.snackyController,
+      snackyUseNavigationObserver: app?.snackyUseNavigationObserver ?? true,
+      supportedLocales: app?.supportedLocales ?? const <Locale>[Locale('en')],
+      title: title,
+      localizationsDelegates: app?.localizationsDelegates,
+      targetPlatform: app?.targetPlatform,
       home: HomeScreen(
         title: title,
         sections: sections,
